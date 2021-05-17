d7fd754afdf31ac6ebc2a448a9a26621043244ad
Rachit Sharma <rs94887@gmail.com>
2021-05-17 19:07:38 +0530

    deleted

--- src/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const mongoDB = process.env.MONGO_DB || ""
-mongoose.connect(`${mongoDB}`, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
-
